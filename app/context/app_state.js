9d273478d4e96ad77f8e5087f0cee9819b4d9d5c
chauzer5 <chauzer.reed@gmail.com>
2023-05-21 18:48:35 -0600

    Auth is now persistent but turning off "remember me" doesn't work yet

--- app/context/app_state.js
+++ app/context/app_state.js
@@ -3,6 +3,7 @@
 import { onAuthStateChanged } from "firebase/auth";
 import {createContext, useContext, useState} from "react";
 import { auth } from "../firebase/firebaseSetup";
+import { useRouter } from "next/navigation";
 
 const AppContext = createContext({
     signedInUserId: null,
@@ -15,14 +16,17 @@ const AppContext = createContext({
 export function AppWrapper({children}) {
     const [signedInUserId, setSignedInUserId] = useState(null);
     const [userFirstName, setUserFirstName] = useState(null);
+    const router = useRouter()
 
     onAuthStateChanged(auth, (user) => {
         if (user) {
             setSignedInUserId(user.uid);
             setUserFirstName(user.displayName);
+            router.push('/home');
         } else {
             setSignedInUserId(null);
             setUserFirstName(null);
+            router.push('/signin');
         }
     });
 
